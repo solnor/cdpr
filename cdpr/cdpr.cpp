2110776cb7eff81ab22e9433ea58ba7b8aacbba9
solnor <solve.norsterud@gmail.com>
2024-05-02 18:18:05 +0200

    Remove prints

--- cdpr/cdpr.cpp
+++ cdpr/cdpr.cpp
@@ -94,7 +94,7 @@ Eigen::Vector4d calculate_f_loss_dir(const Eigen::Ref<const Eigen::Vector4d>& ve
 			map(abs(vels(1)), in_min, in_max, 0, 1),
 			map(abs(vels(2)), in_min, in_max, 0, 1),
 			map(abs(vels(3)), in_min, in_max, 0, 1);
-	std::cout << "velp:\n" << velp << std::endl;
+	//std::cout << "velp:\n" << velp << std::endl;
 	velp << sgn(vels(0))*velp(0),
 			sgn(vels(1))*velp(1),
 			sgn(vels(2))*velp(2),
@@ -376,12 +376,13 @@ int control_loop() {
 
 		AT = calculate_structure_matrix(a, b, q, invkin.betar, r_p);
 		
-		std::cout << "we: \n" << wd << std::endl;
-		std::cout << "q: \n" << q << std::endl;
-		std::cout << "vel: \n" << vel << std::endl;
-		std::cout << "vel_m: \n" << vel_m << std::endl;
+		//std::cout << "we: \n" << wd << std::endl;
+		//std::cout << "q: \n" << q << std::endl;
+		//std::cout << "vel: \n" << vel << std::endl;
+		//std::cout << "vel_m: \n" << vel_m << std::endl;
 		std::cout << "fvel: \n" << fvel << std::endl;
-		std::cout << "f0: \n" << f0 << std::endl;
+		//std::cout << "f0: \n" << f0 << std::endl;
+
 		/*std::cout << "l: \n" << l << std::endl;
 		std::cout << "lfk: \n" << lfk << std::endl;*/
 		//std::cout << "wd: \n" << wd << std::endl;
@@ -404,7 +405,10 @@ int control_loop() {
 		T = (fres.f).cwiseProduct(r_d*(-1)*motor_signs);
 
 		//std::cout << "f_pinv: \n" << f_pinv << std::endl;
-		std::cout << "f: \n" << fres.f << std::endl;
+		
+		//std::cout << "f: \n" << fres.f << std::endl;
+
+
 		//std::cout << "f_pinv_t: \n" << f_pinv_t << std::endl;
 		//std::cout << "vel_m: \n" << vel_m << std::endl;
 		//std::cout << "f0: \n" << f0 << std::endl;
