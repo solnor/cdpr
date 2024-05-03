6fe8bae83a08ae02d600d9a0d513af7f9e0466c4
solnor <solve.norsterud@gmail.com>
2024-05-03 14:36:01 +0200

    Change mapping function arguments

--- cdpr/cdpr.cpp
+++ cdpr/cdpr.cpp
@@ -112,8 +112,8 @@ Eigen::Vector4d calculate_f_loss_dir(const Eigen::Ref<const Eigen::Vector4d>& ve
 			(bool)(ceil(abs(velp(1)))),
 			(bool)(ceil(abs(velp(2)))),
 			(bool)(ceil(abs(velp(3))));*/
-	double in_min = 0.5;
-	double in_max = 1.5;
+	double in_min = 1.0;
+	double in_max = 2.5;
 	velp << map(abs(vels(0)), in_min, in_max, 0, 1),
 			map(abs(vels(1)), in_min, in_max, 0, 1),
 			map(abs(vels(2)), in_min, in_max, 0, 1),
