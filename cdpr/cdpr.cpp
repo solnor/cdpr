2a1e3c0c42e6b39d14145a0fe1ef5f7b8ce2c691
solnor <solve.norsterud@gmail.com>
2024-05-03 15:04:22 +0200

    Change cable force allocation algorithm

--- cdpr/cdpr.cpp
+++ cdpr/cdpr.cpp
@@ -457,8 +457,8 @@ int control_loop() {
 		//std::cout << "AT: \n" << AT << std::endl;
 		
 		//f_ref = (f_min + f_max) / 2;
-		fres.f = f_ref*Eigen::Vector4d::Ones() - AT_pinv*(wd + AT * f_ref*Eigen::Vector4d::Ones());
-		//fres = force_alloc_iterative_slack(AT.transpose(), f_min, f_max, f_ref, f_prev, wd);
+		//fres.f = f_ref*Eigen::Vector4d::Ones() - AT_pinv*(wd + AT * f_ref*Eigen::Vector4d::Ones());
+		fres = force_alloc_iterative_slack(AT.transpose(), f_min, f_max, f_ref, f_prev, wd);
 
 		
 		fs = calculate_fs(vel_m, e, f_static, precv, precx, precy, prect);
